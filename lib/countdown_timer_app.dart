3962a25db94f9111a8eceae8521a2423af403832
Rabindra <drarabin@gmail.com>
2021-10-15 09:41:11 +0545

    first commit

---
+++ lib/countdown_timer_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CountdownTimerApp extends StatelessWidget {
+  const CountdownTimerApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Countdown Timer'),
+      ),
+    );
+  }
+}
